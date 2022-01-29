2dd3bfb3680e7148865c45aa21b0aba9e605c933
ElusiveMind22 <alberto-ionut.toscariu@student.tuiasi.ro>
2022-01-30 01:50:47 +0200

    Initial commit

---
+++ src/main/java/com/example/datajpacourse/DataJpaCourseApplication.java
@@ -0,0 +1,13 @@
+package com.example.datajpacourse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataJpaCourseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DataJpaCourseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/datajpacourse/DataJpaCourseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.datajpacourse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataJpaCourseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
